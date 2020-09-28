b527c7b226df3a7df701ef354e463e34356acd63
Kyaw <70256618+Joe-19-20@users.noreply.github.com>
2020-09-28 17:32:51 +0630

    issue 6

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/course', function () {
+    return view('course');
+});
+
 Route::resource('admin','AdminController');
 Route::resource('courses','CourseController');
 Route::resource('services','ServiceController');
 Route::resource('menus','MenuController');
-Route::resource('services', 'ServiceController');
 Route::resource('categories', 'CategoryController');
