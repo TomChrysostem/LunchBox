bd9a05244fcdb26b39aafa79e3b3167e6f6eaa21
Kyaw <70256618+Joe-19-20@users.noreply.github.com>
2020-10-11 16:32:12 +0630

    add attendees crud

---
+++ app/Http/Controllers/AttendeeController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Attendee;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AttendeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $attendees = Attendee::latest()->paginate(5);
+        return view('admin.attendees.index',compact('attendees'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.attendees.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'date' => 'required',
+            'qty' => 'required',
+            'course_id' => 'required',
+            'messages' => 'required',
+
+        ]);
+        Attendee::create($request->all());
+                        return view('users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Attendee  $attendee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attendee $attendee)
+    {
+        return view('admin.attendees.show',compact('attendee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Attendee  $attendee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Attendee $attendee)
+    {
+        return view('admin.attendees.edit',compact('attendee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Attendee  $attendee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attendee $attendee)
+    {
+        $request->validate([
+            'user_name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'date' => 'required',
+            'qty' => 'required',
+            'course_id' => 'required',
+            'messages' => 'required',
+
+        ]);
+        $attendee->update($request->all());
+        return redirect()->route('attendees.index')
+                        ->with('success','Attendee updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Attendee  $attendee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attendee $attendee)
+    {
+        $attendee->delete();
+        return redirect()->route('attendees.index')
+                        ->with('success','Attendee deleted successfully');
+    }
+}
---
+++ resources/views/admin/attendees/edit.blade.php
@@ -0,0 +1,137 @@
+@extends('admin.layout')
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-lg-12 margin-tb">
+
+            <div class="pull-left">
+
+                <h2>Edit Attendee</h2>
+
+            </div>
+
+            <div class="pull-right">
+
+                <a class="btn btn-primary" href="{{ route('attendees.index') }}"> Back</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    @if ($errors->any())
+
+        <div class="alert alert-danger">
+
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+
+            <ul>
+
+                @foreach ($errors->all() as $error)
+
+                    <li>{{ $error }}</li>
+
+                @endforeach
+
+            </ul>
+
+        </div>
+
+    @endif
+
+  
+
+    <form action="{{ route('attendees.update',$attendee->id) }}" method="POST">
+
+        @csrf
+
+        @method('PUT')
+
+   
+
+         <div class="row">
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="user_name" value="{{ $attendee->user_name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Phone:</strong>
+                    <input type="text" name="phone" value="{{ $attendee->phone }}" class="form-control" placeholder="Phone">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <input type="text" name="email" value="{{ $attendee->email }}" class="form-control" placeholder="Email">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Address:</strong>
+                    <textarea class="form-control" style="height:100px" name="address" placeholder="Address">{{ $attendee->address }}</textarea>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <label class="control-label col-2">Date:</label>
+                <div class="col-10">
+                   <input type="text" name="date" class="form-control" id="datetimepicker" value="{{$attendee->date}}"/> 
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Quantity:</strong>
+                    <input type="text" name="qty" value="{{ $attendee->qty }}" class="form-control" placeholder="Numbers of Students">
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Course item:</strong>
+                    <select class="form-control" name="course_id" value="{{$attendee->course_id}}">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Message:</strong>
+                    <textarea class="form-control" style="height:150px" name="messages" placeholder="Message">{{ $attendee->messages }}</textarea>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+
+            </div>
+
+        </div>
+    </form>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js" integrity="sha256-5YmaxAwMjIpMrVlK84Y/+NjCpKnFYa8bWWBbUHSBGfU=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $(function () {
+        $('#datetimepicker').datepicker();
+    });
+</script>
+
+@endsection
